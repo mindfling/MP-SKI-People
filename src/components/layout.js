b7d1e28ab1b2ca506d9eb6e310d713590242db26
Nepochtov Dmitry <dimendroider@gmail.com>
2025-04-28 00:01:13 +0300

    Create layout.js

---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+export const layout = (child, classList) => {
+
+  const el = document.createElement('div');
+  el.classList.add('container');
+
+  if (classList) {
+    el.classList.add(classList);
+  }
+  el.innerHTML = child;
+
+  return el;
+}
