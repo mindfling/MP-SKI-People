f7c8e8b03856bc962d1df22c7181a45026cda553
Nepochtov Dmitry <dimendroider@gmail.com>
2025-05-08 23:44:38 +0300

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,22 @@
-// * layout.js 
+// * layout.js
 // Контейнер конпонент .container
 // child html содержимое контейнера
 // classList список дополнительных классов
 
-export const layout = (child, classList) => {
-
+export const layout = (childhtml, classList) => {
   // создаем элемент div
-  const el = document.createElement('div');
-  
+  const el = document.createElement("div");
+
   // добавляем класс .container
-  el.classList.add('container');
-  
+  el.classList.add("container");
+
   // добавляем дополнительные классы если есть
   if (classList) {
     el.classList.add(classList);
   }
-  
+
   // вставляем текстовый код html внутрь контейнреа
-  el.innerHTML = child;
+  el.innerHTML = childhtml;
 
   return el;
-}
+};
