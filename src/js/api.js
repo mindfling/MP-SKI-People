c49b18a44c2e1d312f9c03037209d53a0a49f3a2
Nepochtov Dmitry <dimendroider@gmail.com>
2025-05-09 10:58:28 +0300

    update fix

--- src/js/api.js
+++ src/js/api.js
@@ -4,7 +4,6 @@ import { API_URL } from "./const";
 export const getData = async () => {
   try {
     const response = await fetch(API_URL);
-    // const result = JSON.parse(response);
     const result = await response.json();
     return result;
 
@@ -16,9 +15,6 @@ export const getData = async () => {
 export const loadData = async () => {
   return await fetch(API_URL)
     .then((data) => {
-      // console.log("fetch data: ", data);
-      // console.log(JSON.stringify(data));
-      // const result = JSON.parse(data);
       return data.json();
     })
     .then((obj) => {
--- src/js/router.js
+++ src/js/router.js
@@ -14,25 +14,28 @@ export const initRouter = () => {
   // my init router
   console.log("init router 😃");
 
+  const href = window.location.href;
+  const url = document.URL;
+  // console.log('url page is', href, url);
+  
   router
-    .on('/', async () => {
+  .on('/', async () => {
       console.log('Главная страница');
       const goods = await getData();
-      
       header();
-      // main(catalog());
       catalog(main(), goods);
       productList('Список товаров', goods, main());
       footer();
+      router.updatePageLinks();
     })
     
     .on('/favorite', async () => {
       console.log("Favorite⭐ page Избранное");
       const goods = await loadData();
-
       header();
       productList('Избранное', goods, main());
       footer();
+      router.updatePageLinks();
     })
 
     .on('/product', async () => {
@@ -40,34 +43,37 @@ export const initRouter = () => {
 
       header();
       footer();
+      router.updatePageLinks();
     })
 
     .on('/cart', async () => {
         console.log("Cart🛒 Корзина");
-        // const goods = await getData();
+        const goods = await getData();
         // const data = localStorageLoad('ski-people-cart');
         header();
         // cart(`Корзина товаров`, main(), data)
-        footer();
         router.updatePageLinks();
+        footer();
       },
-      // {
-      //   leave(done) {
-      //     console.log('leave Оставляем корзину');
-      //     // productList('remove');
-      //     done();
-      //   }
-      // }
+      {
+        leave(done) {
+          console.log('leave Оставляем корзину');
+          productList('remove');
+          done();
+        }
+      }
     )
 
     .notFound(() => {
-      console.log("page 404");
+      console.log("🤖 page 404");
       document.body.innerHTML = `
         <main class="main">
-          <div class="container page__notfound">
-            <h1 class="page__title">Title 404 PAGE</h1>
+          <div class="container page__notfound" style="justify-content:center;padding:50px;">
+            <h1 class="page__title" style="text-align:center;font-size:54px;font-family:cursive;">Title 404 PAGE</h1>
           </div>
         </main>`;
+      header();
+      footer();
     });
 
   router.resolve();
