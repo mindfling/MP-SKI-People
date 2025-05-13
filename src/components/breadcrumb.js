1e4c72916a10f4d8a57a34db7ed1ff8bbd44b507
Nepochtov Dmitry <dimendroider@gmail.com>
2025-05-13 23:28:56 +0300

    Update breadcrumb.js

--- src/components/breadcrumb.js
+++ src/components/breadcrumb.js
@@ -1,1 +1,34 @@
 // breadcrumb.js ХЛЕБНЫЕ КРОШКИ
+
+export const breadcrumb = (action, parent) => {
+
+  const el = document.createElement('div');
+
+  const child = `
+    <!-- ХЛЕБНЫЕ КРОШКИ -->
+    
+    <div class="breadcrumb">
+      <div class="container breadcrumb__container">
+        <nav class="breadcrumb__navigation">
+          <ul class="breadcrumb__list">
+            <li class="breadcrumb__item">
+              <a class="breadcrumb__link" href="#" title="Вернуться на Главную">Главная</a>
+            </li>
+
+            <li class="breadcrumb__item">
+              <a class="breadcrumb__link" href="#" title="Вернуться на категорию Лыжи">Лыжи</a>
+            </li>
+
+            <li class="breadcrumb__item">
+              <a class="breadcrumb__link" href="#" title="Вернуться на категорию Горные лыжи">Горные лыжи</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  `;
+
+  el.innerHTML = child;
+
+  return el;
+}
