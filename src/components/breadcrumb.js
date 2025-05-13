067375407b780cd5283ca1724ec806c265f8e7bf
Nepochtov Dmitry <dimendroider@gmail.com>
2025-05-14 00:06:10 +0300

    update dim

--- src/components/breadcrumb.js
+++ src/components/breadcrumb.js
@@ -1,34 +1,48 @@
 // breadcrumb.js ХЛЕБНЫЕ КРОШКИ
+import { layout } from "./layout";
+
+
+let rendered = false;
 
 export const breadcrumb = (action, parent) => {
 
+  if ( action === 'remove' ) {
+    document.querySelector('.breadcrumb');
+    rendered = false;
+    return '';
+  }
+
+  if ( rendered ) {
+    return document.querySelector('.breadcrumb');
+  }
+
+
   const el = document.createElement('div');
+  el.classList.add('breadcrumb');
 
   const child = `
     <!-- ХЛЕБНЫЕ КРОШКИ -->
-    
-    <div class="breadcrumb">
-      <div class="container breadcrumb__container">
-        <nav class="breadcrumb__navigation">
-          <ul class="breadcrumb__list">
-            <li class="breadcrumb__item">
-              <a class="breadcrumb__link" href="#" title="Вернуться на Главную">Главная</a>
-            </li>
+    <nav class="breadcrumb__navigation">
+      <ul class="breadcrumb__list">
+        <li class="breadcrumb__item">
+          <a class="breadcrumb__link" href="#" title="Вернуться на Главную">Главная</a>
+        </li>
 
-            <li class="breadcrumb__item">
-              <a class="breadcrumb__link" href="#" title="Вернуться на категорию Лыжи">Лыжи</a>
-            </li>
+        <li class="breadcrumb__item">
+          <a class="breadcrumb__link" href="#" title="Вернуться на категорию Лыжи">Лыжи</a>
+        </li>
 
-            <li class="breadcrumb__item">
-              <a class="breadcrumb__link" href="#" title="Вернуться на категорию Горные лыжи">Горные лыжи</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
+        <li class="breadcrumb__item">
+          <a class="breadcrumb__link" href="#" title="Вернуться на категорию Горные лыжи">Горные лыжи</a>
+        </li>
+      </ul>
+    </nav>
   `;
 
-  el.innerHTML = child;
+  el.append(layout(child, 'breadcrumb__container'));
+  parent.append(el);
+
+  rendered = true;
 
   return el;
 }
